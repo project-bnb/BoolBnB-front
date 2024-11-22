6eb0a32037614a6a87eea81b0e57ac9261429f24
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-11-22 12:55:51 +0100

    migliorato leggermente esteticamente il form create

--- src/views/CreationPage.vue
+++ src/views/CreationPage.vue
@@ -42,44 +42,62 @@ export default {
   <div class="bg-yellow-400 w-1/2 rounded-lg my-7 p-5 mx-auto text-left">
     <form @submit.prevent="createApartment" action="">
       <div class="py-4">
-        <label for="title">Titolo</label>
-        <input class="shadow-md rounded" required type="text" v-model="form.title">
+        <div>
+          <label for="title">Titolo</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="text" placeholder="Inserisci un titolo..." v-model="form.title">
       </div>
       <div class="py-4">
-        <label for="rooms">Stanze</label>
-        <input class="shadow-md rounded" required type="number" v-model="form.rooms">
+        <div>
+          <label for="rooms">Stanze</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="number" placeholder="Inserisci il numero delle stanze..." v-model="form.rooms">
       </div>
       <div class="py-4">
-        <label for="beds">Letti</label>
-        <input class="shadow-md rounded" required type="number" v-model="form.beds">
+        <div>
+          <label for="beds">Letti</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="number" placeholder="Inserisci il numero di letti..." v-model="form.beds">
       </div>
       <div class="py-4">
-        <label for="bathrooms">Bagni</label>
-        <input class="shadow-md rounded" required type="number" v-model="form.bathrooms">
+        <div>
+          <label for="bathrooms">Bagni</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="number" placeholder="Inserisci il numero di bagni..." v-model="form.bathrooms">
       </div>
       <div class="py-4">
-        <label for="square_meters">Metri quadri</label>
-        <input class="shadow-md rounded" required type="number" v-model="form.square_meters">
+        <div>
+          <label for="square_meters">Metri quadri</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="number" placeholder="Inserisci i metri quadri..." v-model="form.square_meters">
       </div>
       <div class="py-4">
-        <label for="address">Indirizzo</label>
-        <input class="shadow-md rounded" required type="text" v-model="form.address">
+        <div>
+          <label for="address">Indirizzo</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="text" placeholder="Inserisci l'indirizzo..." v-model="form.address">
       </div>
       <div class="py-4">
-        <label for="latitude">Latitudine</label>
-        <input class="shadow-md rounded" type="text" v-model="form.latitude">
+        <div>
+          <label for="latitude">Latitudine</label>
+        </div>
+        <input class="w-full shadow-md rounded" type="text" placeholder="Inserisci la latitudine..." v-model="form.latitude">
       </div>
       <div class="py-4">
-        <label for="longitude">Longitudine</label>
-        <input class="shadow-md rounded" type="text" v-model="form.longitude">
+        <div>
+          <label for="longitude">Longitudine</label>
+        </div>
+        <input class="w-full shadow-md rounded" type="text" placeholder="Inserisci la longitudine..." v-model="form.longitude">
       </div>
       <div class="py-4">
-        <label for="image">Immagine (URL/Path)</label>
-        <input class="shadow-md rounded" required type="text" v-model="form.image" />
+        <div>
+          <label for="image">Immagine (URL/Path)</label>
+        </div>
+        <input class="w-full shadow-md rounded" required type="text" placeholder="Inserisci un'immagine dell'appartamento..." v-model="form.image" />
       </div>
       <div class="py-4">
         <label for="is_visible">Visibile</label>
-        <input class="shadow-md rounded" type="checkbox" v-model="form.is_visible">
+        <input class="shadow-md rounded mx-3" type="checkbox" placeholder="" v-model="form.is_visible">
       </div>
 
       <!-- BOTTONE SUBMIT -->

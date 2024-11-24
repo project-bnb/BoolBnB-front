a1db657362330bf88e340414410eac9f1a6adf45
surefire-hu <surefire.hu2001@gmail.com>
2024-11-24 18:41:59 +0100

    creato ApartmentShow

--- src/views/ApartmentShow.vue
+++ src/views/ApartmentShow.vue
@@ -1,20 +1,3 @@
-<template>
-  <div class="max-w-4xl mx-auto p-8 mt-10">
-    <h1 class="text-3xl font-bold text-gray-800 mb-6 text-center">{{ apartment.title }}</h1>
-    <img :src="apartment.image" alt="Property Image" class="w-full h-64 object-cover mb-4 rounded" />
-    <div class="mb-4">
-      <p class="text-lg"><strong>Stanze:</strong> {{ apartment.rooms }}</p>
-      <p class="text-lg"><strong>Letti:</strong> {{ apartment.beds }}</p>
-      <p class="text-lg"><strong>Bagni:</strong> {{ apartment.bathrooms }}</p>
-      <p class="text-lg"><strong>Metri Quadri:</strong> {{ apartment.square_meters }} m²</p>
-      <p class="text-lg"><strong>Indirizzo:</strong> {{ apartment.address }}</p>
-      <p class="text-lg"><strong>Coordinate:</strong> {{ apartment.latitude }}, {{ apartment.longitude }}</p>
-      <p class="text-lg"><strong>Visibilità:</strong> {{ apartment.is_visible ? 'Visibile' : 'Non Visibile' }}</p>
-    </div>
-    <router-link to="/apartment-list" class="text-blue-500 hover:underline">Torna alla lista degli appartamenti</router-link>
-  </div>
-</template>
-
 <script>
 import axios from 'axios';
 
@@ -43,3 +26,21 @@ export default {
   },
 };
 </script>
+
+<template>
+  <div class="max-w-4xl mx-auto p-8 mt-10">
+    <h1 class="text-3xl font-bold text-gray-800 mb-6 text-center">{{ apartment.title }}</h1>
+    <img :src="apartment.image" alt="Property Image" class="w-full h-64 object-cover mb-4 rounded" />
+    <div class="mb-4">
+      <p class="text-lg"><strong>Stanze:</strong> {{ apartment.rooms }}</p>
+      <p class="text-lg"><strong>Letti:</strong> {{ apartment.beds }}</p>
+      <p class="text-lg"><strong>Bagni:</strong> {{ apartment.bathrooms }}</p>
+      <p class="text-lg"><strong>Metri Quadri:</strong> {{ apartment.square_meters }} m²</p>
+      <p class="text-lg"><strong>Indirizzo:</strong> {{ apartment.address }}</p>
+      <p class="text-lg"><strong>Coordinate:</strong> {{ apartment.latitude }}, {{ apartment.longitude }}</p>
+      <p class="text-lg"><strong>Visibilità:</strong> {{ apartment.is_visible ? 'Visibile' : 'Non Visibile' }}</p>
+    </div>
+    <router-link to="/apartment-list" class="text-blue-500 hover:underline">Torna alla lista degli appartamenti</router-link>
+  </div>
+</template>
+

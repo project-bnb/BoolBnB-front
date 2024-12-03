fd241506b20529638b62642da9dd56a65167faeb
MindfulLearner <dimajosh1009@gmail.com>
2024-12-03 22:32:10 +0000

    fix/login

--- src/components/ApartmentList.vue
+++ src/components/ApartmentList.vue
@@ -36,7 +36,7 @@ export default {
   methods: {
     getApartments() {
       axios
-        .get('http://192.168.1.101:9000/api/apartments')
+        .get('http://127.0.0.1:8000/api/apartments')
         .then((res) => {
           this.apartments = res.data.data.filter(
             (apartment) =>

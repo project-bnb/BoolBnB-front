dc6c98813e3c41acd6886af116b0ff04f2f91db4
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-12-05 11:43:04 +0100

    cambio server

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -23,7 +23,7 @@ export default {
     }
   },
   mounted() {
-    axios.get('http://192.168.1.101:9000/api/user', { withCredentials: true })
+    axios.get('http://127.0.0.1:8000/api/user', { withCredentials: true })
     .then(response => {
       this.user = response.data;
       this.isAuthenticated = true;
